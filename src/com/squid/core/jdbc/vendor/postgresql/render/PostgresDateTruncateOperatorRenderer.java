54b202cfca23b50fd4eff34023acb3ee707e460f
Julien Theulier <julien@squidsolutions.com>
2016-09-02 13:47:19 +0200

    T1831: extend date_truncate with quarter period

--- src/com/squid/core/jdbc/vendor/postgresql/render/PostgresDateTruncateOperatorRenderer.java
+++ src/com/squid/core/jdbc/vendor/postgresql/render/PostgresDateTruncateOperatorRenderer.java
@@ -2,12 +2,12 @@
  * Copyright © Squid Solutions, 2016
  *
  * This file is part of Open Bouquet software.
- *  
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation (version 3 of the License).
  *
- * There is a special FOSS exception to the terms and conditions of the 
+ * There is a special FOSS exception to the terms and conditions of the
  * licenses as they are applied to this program. See LICENSE.txt in
  * the directory of this program distribution.
  *
@@ -38,19 +38,23 @@ import com.squid.core.sql.render.SQLSkin;
 public class PostgresDateTruncateOperatorRenderer extends DateTruncateOperatorRenderer {
 
 	@Override
-	protected String prettyPrintTwoArgs(SQLSkin skin, OperatorPiece piece, OperatorDefinition opDef, String[] args) throws RenderingException {
-		//ExtendedType[] extendedTypes = getExtendedPieces(piece);
-		if(DateTruncateOperatorDefinition.WEEK.equals(args[1].replaceAll("'", ""))) {
-	        return "CAST(DATE_TRUNC('week', "+args[0]+") AS DATE)";
-	    } else if(DateTruncateOperatorDefinition.MONTH.equals(args[1].replaceAll("'", ""))) {
-	        return "CAST(DATE_TRUNC('month', "+args[0]+") AS DATE)";
-	    } else if(DateTruncateOperatorDefinition.YEAR.equals(args[1].replaceAll("'", ""))) {
-	        return "CAST(DATE_TRUNC('year', "+args[0]+") AS DATE)";
-	    } else {
-	    	// Even if it's a date, we have to truncate it and transform to a date in order to have a good rollup between day & week
-	    	// Bug is that weeks are not displayed in the result set
-	        return "CAST(DATE_TRUNC('day', "+args[0]+") AS DATE)";
-	    }
+	protected String prettyPrintTwoArgs(SQLSkin skin, OperatorPiece piece, OperatorDefinition opDef, String[] args)
+			throws RenderingException {
+		// ExtendedType[] extendedTypes = getExtendedPieces(piece);
+		if (DateTruncateOperatorDefinition.WEEK.equals(args[1].replaceAll("'", ""))) {
+			return "CAST(DATE_TRUNC('week', " + args[0] + ") AS DATE)";
+		} else if (DateTruncateOperatorDefinition.MONTH.equals(args[1].replaceAll("'", ""))) {
+			return "CAST(DATE_TRUNC('month', " + args[0] + ") AS DATE)";
+		} else if (DateTruncateOperatorDefinition.QUARTER.equals(args[1].replaceAll("'", ""))) {
+			return "CAST(DATE_TRUNC('quarter', " + args[0] + ") AS DATE)";
+		} else if (DateTruncateOperatorDefinition.YEAR.equals(args[1].replaceAll("'", ""))) {
+			return "CAST(DATE_TRUNC('year', " + args[0] + ") AS DATE)";
+		} else {
+			// Even if it's a date, we have to truncate it and transform to a
+			// date in order to have a good rollup between day & week
+			// Bug is that weeks are not displayed in the result set
+			return "CAST(DATE_TRUNC('day', " + args[0] + ") AS DATE)";
+		}
 	}
 
 }
