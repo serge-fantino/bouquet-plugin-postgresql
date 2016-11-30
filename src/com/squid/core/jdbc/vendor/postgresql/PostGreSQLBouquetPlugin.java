46575492b600840ac08efbb4021a9eaa6484028f
hoahd <hoa@squidsolutions.com>
2016-11-30 15:02:44 +0100

    T2085

--- src/com/squid/core/jdbc/vendor/postgresql/PostGreSQLBouquetPlugin.java
+++ src/com/squid/core/jdbc/vendor/postgresql/PostGreSQLBouquetPlugin.java
@@ -14,21 +14,21 @@ public class PostGreSQLBouquetPlugin extends BaseBouquetPlugin {
 	@Override
 	public void loadDriver() {
 
-		// get the current plugin jar 
+		// get the current plugin jar
 		URL[] paths = new URL[1];
 		paths[0] = this.getClass().getProtectionDomain().getCodeSource().getLocation();
 
 		// load the driver within an isolated classLoader
-		URLClassLoader cl = new URLClassLoader(paths);
+		this.driverCL = new URLClassLoader(paths);
 		ClassLoader rollback = Thread.currentThread().getContextClassLoader();
-		Thread.currentThread().setContextClassLoader(cl);
+		Thread.currentThread().setContextClassLoader(driverCL);
 
-		ServiceLoader<Driver> sl = ServiceLoader.load(java.sql.Driver.class, cl) ;
-		Iterator<Driver> driversIter = sl.iterator() ;
+		ServiceLoader<Driver> sl = ServiceLoader.load(java.sql.Driver.class, driverCL);
+		Iterator<Driver> driversIter = sl.iterator();
 		this.drivers = new ArrayList<Driver>();
-	
-		while (driversIter.hasNext()){
-			drivers.add(driversIter.next());			
+
+		while (driversIter.hasNext()) {
+			drivers.add(driversIter.next());
 		}
 		Thread.currentThread().setContextClassLoader(rollback);
 	}
